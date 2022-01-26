67c3b91248e0e7a43d4a2ce98e7ff33ec076f4b4
Bavon101 <bavonfate@gmail.com>
2022-01-26 12:02:51 +0300

    Add: Desktop project details view logic

--- js/main.js
+++ js/main.js
@@ -27,10 +27,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image.svg', './src/img/svg/project_image.svg', './src/img/svg/project_image.svg', './src/img/svg/project_image.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'project_card_macro',
+    details_card: 'project_details_card',
+    project_name: ['project_name'],
+  },
 },
 {
   name: 'Bavon Portfolio ',
@@ -39,10 +44,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image_1.svg', './src/img/svg/project_image_1.svg', './src/img/svg/project_image_1.svg', './src/img/svg/project_image_1.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'project_image_1',
+    details_card: 'project_details_card_mini_1',
+    project_name: ['project_name', 'project_name_mini'],
+  },
 },
 {
   name: 'Bavon Portfolio ',
@@ -51,10 +61,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image_2.svg', './src/img/svg/project_image_2.svg', './src/img/svg/project_image_2.svg', './src/img/svg/project_image_2.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'project_image_2',
+    details_card: 'project_details_card_mini_2',
+    project_name: ['project_name', 'project_name_mini'],
+  },
 },
 {
   name: 'Bavon Portfolio ',
@@ -63,10 +78,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image_3.svg', './src/img/svg/project_image_3.svg', './src/img/svg/project_image_3.svg', './src/img/svg/project_image_3.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'desktop_image_geometry_4',
+    details_card: 'project_details_card_mini_5',
+    project_name: ['project_name', 'project_name_mini'],
+  },
 },
 {
   name: 'Bavon Portfolio ',
@@ -75,10 +95,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image_4.svg', './src/img/svg/project_image_4.svg', './src/img/svg/project_image_4.svg', './src/img/svg/project_image_4.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'project_image_4',
+    details_card: 'project_details_card_mini_4',
+    project_name: ['project_name', 'project_name_mini'],
+  },
 },
 {
   name: 'Bavon Portfolio ',
@@ -87,10 +112,15 @@ const projects = [{
           velit consequatur, inventore deserunt, ad fugiat! Ab qui vitae
           laudantium velit tenetur tempore temporibus ex in dolorem nulla! Enim,
           adipisci in!`,
-  images: ['', '', '', ''],
+  images: ['./src/img/svg/project_image_5.svg', './src/img/svg/project_image_5.svg', './src/img/svg/project_image_5.svg', './src/img/svg/project_image_5.svg'],
   technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
   live_link: '#',
   source_link: '#',
+  desktop_data: {
+    container: 'project_image_5',
+    details_card: 'project_details_card_mini',
+    project_name: ['project_name', 'project_name_mini'],
+  },
 },
 ];
 
@@ -149,7 +179,57 @@ function appendProjects() {
   }
 }
 
+function appendDesktopProjects() {
+  const projectsSection = document.getElementById('Portfolio');
+  const desktopProjectsImagesList = document.getElementById('destop_projects_list');
+  for (let i = 0; i < projects.length; i += 1){
+    const projectCard = document.createElement('div');
+    projectCard.classList.add(projects[i].desktop_data.container);
+    const detailsCard = document.createElement('div');
+    detailsCard.classList.add(projects[i].desktop_data.details_card);
+    const projectName = document.createElement('h2');
+    projectName.classList.add(...projects[i].desktop_data.project_name);
+    const projectNameText = document.createTextNode(projects[i].name);
+    projectName.appendChild(projectNameText);
+    detailsCard.appendChild(projectName);
+    const projectTecContainer = document.createElement('div');
+    projectTecContainer.classList.add('project_tecs_container');
+    const techList = document.createElement('ul');
+    techList.classList.add('ul');
+    projects[i].technologies.forEach((t) => { 
+      const techItem = document.createElement('li');
+      techItem.classList.add('li');
+      const techName = document.createElement('p');
+      techName.classList.add('tech_text');
+      const techNameText = document.createTextNode(t);
+      techName.appendChild(techNameText);
+      techItem.appendChild(techName);
+      techList.appendChild(techItem);
+    });
+    projectTecContainer.appendChild(techList);
+    detailsCard.appendChild(projectTecContainer);
+    const seeProjectBtn = document.createElement('button');
+    seeProjectBtn.classList.add('see_project');
+    seeProjectBtn.setAttribute('id', `see_desktop_pop_${i}`);
+    const btnText = document.createTextNode('See this project');
+    seeProjectBtn.appendChild(btnText);
+    const btnIcon = document.createElement('i');
+    btnIcon.classList.add('arrow_forward');
+    seeProjectBtn.appendChild(btnIcon);
+    detailsCard.appendChild(seeProjectBtn);
+    projectCard.appendChild(detailsCard);
+    if (i === 0) {
+      projectsSection.appendChild(projectCard);
+    } else {
+      desktopProjectsImagesList.appendChild(projectCard);
+    }
+
+  }
+  projectsSection.appendChild(desktopProjectsImagesList);
+}
+
 appendProjects();
+appendDesktopProjects();
 const projectsButton = document.querySelector('.see_project');
 const detailsCard = document.getElementById('view_project');
 const vanishPoint = document.querySelectorAll('.can_dissapear');
@@ -166,7 +246,7 @@ function closeDetailsPop() {
   detailsCard.replaceChildren();
 }
 
-function createProjectDetails(id) {
+function createProjectDetails(id,desktop) {
   const project = projects[id];
   if (project === null || project === undefined) {
     return;
@@ -195,6 +275,7 @@ function createProjectDetails(id) {
   imagesContainer.classList.add('project_images_container');
   const mainProjectImage = document.createElement('div');
   mainProjectImage.classList.add('main_project_image');
+  mainProjectImage.style.backgroundImage = `url(${project.images[0]})`;
   const backIcon = document.createElement('button');
   backIcon.classList.add('project_navigation_button_left');
   mainProjectImage.appendChild(backIcon);
@@ -206,7 +287,8 @@ function createProjectDetails(id) {
   imagesCollection.classList.add('project_image_collection');
   project.images.forEach((i) => {
     const smallImage = document.createElement('div');
-    smallImage.classList.add(`project_small_image${i}`);
+    smallImage.classList.add(`project_small_image`);
+    smallImage.style.backgroundImage = `url(${i})`;
     imagesCollection.appendChild(smallImage);
   });
   imagesContainer.appendChild(imagesCollection);
@@ -216,6 +298,8 @@ function createProjectDetails(id) {
   const descriptionText = document.createTextNode(project.description);
   projectDescription.appendChild(descriptionText);
   projectDataCard.appendChild(projectDescription);
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('pop_up_btn_container');
   const viewLiveButton = document.createElement('button');
   viewLiveButton.classList.add('reversed_button');
   const viewBtnText = document.createTextNode('See live');
@@ -223,7 +307,7 @@ function createProjectDetails(id) {
   const viewBtnIcon = document.createElement('i');
   viewBtnIcon.classList.add('project_live_btn');
   viewLiveButton.appendChild(viewBtnIcon);
-  projectDataCard.appendChild(viewLiveButton);
+  btnContainer.appendChild(viewLiveButton);
   const viewSourceBtn = document.createElement('button');
   viewSourceBtn.classList.add('reversed_button');
   const viewSourceTxt = document.createTextNode('See source');
@@ -231,17 +315,45 @@ function createProjectDetails(id) {
   const viewSourceIcon = document.createElement('i');
   viewSourceIcon.classList.add('project_github_btn');
   viewSourceBtn.appendChild(viewSourceIcon);
-  projectDataCard.appendChild(viewSourceBtn);
+  btnContainer.appendChild(viewSourceBtn);
+  projectDataCard.appendChild(btnContainer);
+  if (desktop) {
+    const controlsContainer = document.createElement('div');
+    controlsContainer.classList.add('project_view_controll_container');
+    const prevIconbtn = document.createElement('div');
+    prevIconbtn.classList.add('icon_btn')
+    const previousBtn = document.createElement('button');
+    previousBtn.classList.add('project_contols_btn');
+    const previousBtnIcon = document.createElement('i');
+    previousBtnIcon.classList.add('back_icon');
+    prevIconbtn.appendChild(previousBtnIcon);
+    const previousBtnText = document.createTextNode('Previous project');
+    previousBtn.appendChild(previousBtnText);
+    prevIconbtn.appendChild(previousBtn);
+    controlsContainer.appendChild(prevIconbtn);
+    const nextIconBtn = document.createElement('div');
+    nextIconBtn.classList.add('icon_btn');
+    const nextBtn = document.createElement('button');
+    nextBtn.classList.add('project_contols_btn');
+     const nxtBtnText = document.createTextNode('Next project');
+     nextBtn.appendChild(nxtBtnText);
+     nextIconBtn.appendChild(nextBtn);
+    const nxtBtnIcon = document.createElement('i');
+    nxtBtnIcon.classList.add('forward_icon');
+    nextIconBtn.appendChild(nxtBtnIcon);
+    controlsContainer.appendChild(nextIconBtn);
+    projectDataCard.appendChild(controlsContainer);
+  }
   detailsCard.appendChild(projectDataCard);
   togleVanishPoint();
 }
 
 function onProjectClick(id) {
   const buttonId = id;
-  const projectId = buttonId.replace('project_', '');
+  const projectId = buttonId.replace('project_', '').replace('see_desktop_pop_','');
   const index = parseInt(projectId, 10);
   if (index !== undefined) {
-    createProjectDetails(index);
+    createProjectDetails(index, id.includes('see_desktop_pop_'));
     detailsCard.style.display = 'block';
   }
 }
@@ -250,7 +362,7 @@ projectsButton.addEventListener('click', onProjectClick, false);
 function listenOnBody(event) {
   if (event.target.id === 'close_project_details') {
     closeDetailsPop();
-  } else if (event.target.id.includes('project_')) {
+  } else if (event.target.id.includes('project_') || event.target.id.includes('see_desktop_pop_')) {
     onProjectClick(event.target.id);
   }
 }
